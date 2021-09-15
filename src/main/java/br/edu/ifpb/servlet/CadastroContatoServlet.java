0343b41cb60801e1ed3e91c91f6a3c21c07725fc
italoneves <neves.italo@gmail.com>
2021-09-14 21:55:59 -0300

    Servlet para cadastro concluído

---
+++ src/main/java/br/edu/ifpb/servlet/CadastroContatoServlet.java
@@ -0,0 +1,70 @@
+package br.edu.ifpb.servlet;
+
+import br.edu.ifpb.domain.Contato;
+import br.edu.ifpb.domain.Endereco;
+import br.edu.ifpb.domain.Usuario;
+import br.edu.ifpb.repository.UsuarioRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/cadastrarContato")
+public class CadastroContatoServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        String nome = request.getParameter("nome");
+        String rg = request.getParameter("rg");
+        String cpf = request.getParameter("cpf");
+        String rua = request.getParameter("rua");
+        String numero = request.getParameter("numero");
+        String complemento = request.getParameter("complemento");
+        String bairro = request.getParameter("bairro");
+        String cep = request.getParameter("cep");
+        String cidade = request.getParameter("cidade");
+        String uf = request.getParameter("uf");
+
+        //System.out.println(" rg" + rg + " cpf" + cpf + " nome" +  nome);
+
+        if(cep.isBlank()|| rua.isBlank() || numero.isBlank()
+                || bairro.isBlank() || cidade.isBlank() || uf.isBlank()) {
+            System.out.println("teste" );
+            RequestDispatcher rd = request.getRequestDispatcher("/invalidAdress.jsp");
+
+        }
+        else {
+            Integer rgInteger = Integer.valueOf(rg);
+            Integer cpfInteger = Integer.valueOf(cpf);
+            Integer cepInteger = Integer.valueOf(cep);
+            Integer numeroInteger = Integer.valueOf(numero);
+
+            Endereco endereco = new Endereco(rua,numeroInteger,complemento,bairro,cepInteger,cidade,uf);
+            Contato contato = new Contato(nome,rgInteger,cpfInteger,endereco);
+
+            String emailLog = (String) request.getSession().getAttribute("emailLog");
+            UsuarioRepository usuarioRepository = UsuarioRepository.getInstance();
+            Usuario u = usuarioRepository.findByEmail(emailLog);
+            u.addContato(contato);
+            u.getContatos().stream().forEach(System.out::println);
+            response.sendRedirect("/listaContatos");
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
