c458e225eae8d0f1958dca30db8ced213ef5cab6
italoneves <neves.italo@gmail.com>
2021-09-19 22:33:09 -0300

    Correção de idendereço -> idcontato

--- src/main/java/br/edu/ifpb/repository/UsuarioRepository.java
+++ src/main/java/br/edu/ifpb/repository/UsuarioRepository.java
@@ -25,29 +25,29 @@ public class UsuarioRepository {
         Usuario usuario2 = new Usuario(usuarioId ++, "Érica Clementino", "ericaclementino@gmail.com", "123456");
         Usuario usuario3 = new Usuario(usuarioId ++, "Ítalo Neves", "italo.neves@gmail.com", "123456");
 
-        Endereco endereco = new Endereco(contatoId ++,
+        Endereco endereco = new Endereco(enderecoId ++,
                 "Aurealuz Maciel de Lima", 181, "Casa", "Três Irmãs",
                 58423163, "Campina Grande", "PB");
-        Contato contato = new Contato(enderecoId ++, "Henrique Brito Henrique Brito Henrique Brito Henrique Brito", 123456, 123456, endereco);
+        Contato contato = new Contato(contatoId ++, "Henrique Brito Henrique Brito Henrique Brito Henrique Brito", 123456, 123456, endereco);
         usuario.addContato(contato);
 
-        Endereco endereco1 = new Endereco(contatoId ++,
+        Endereco endereco1 = new Endereco(enderecoId ++,
                 "Aurealuz Maciel de Lima", 184, "Casa", "Três Irmãs",
                 58423163, "Campina Grande", "PB");
-        Contato contato1 = new Contato(enderecoId ++, "Joaquim", 123456, 123457, endereco1);
+        Contato contato1 = new Contato(contatoId ++, "Joaquim", 123456, 123457, endereco1);
         usuario1.addContato(contato1);
 
-        Endereco endereco2 = new Endereco(contatoId ++,
+        Endereco endereco2 = new Endereco(enderecoId ++,
                 "Aurealuz Maciel de Lima", 186, "Casa", "Três Irmãs",
                 58423163, "Campina Grande", "PB");
-        Contato contato2 = new Contato(enderecoId ++, "Henrique Brito", 123456, 123458, endereco2);
+        Contato contato2 = new Contato(contatoId ++, "Henrique Brito", 123456, 123458, endereco2);
         usuario.addContato(contato2);
         usuario1.addContato(contato1);
 
-        Endereco endereco3 = new Endereco(contatoId ++,
+        Endereco endereco3 = new Endereco(enderecoId ++,
                 "Aurealuz Maciel de Lima", 183, "Casa", "Três Irmãs",
                 58423163, "Campina Grande", "PB");
-        Contato contato3 = new Contato(enderecoId ++, "Italo Neves", 123456, 123459, endereco3);
+        Contato contato3 = new Contato(contatoId ++, "Italo Neves", 123456, 123459, endereco3);
         usuario3.addContato(contato3);
 
         usuarios.add(usuario);
@@ -91,6 +91,8 @@ public class UsuarioRepository {
         }
         return false;
     }
+
+
 
     public static Integer getUsuarioId() {
         return usuarioId ++;
